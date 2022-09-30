cd3de94b69842a13dec4ab7ac5e21fdb8701673a
Leoni Weisen <leoni.weisen@tu-dortmund.de>
2022-09-30 16:49:15 +0200

    beschrifteter Graph

--- exp.py
+++ exp.py
@@ -3,6 +3,10 @@ import numpy as np
 
 x = np.linspace(0, 10, 1000)
 
-plt.plot(x, np.exp(x), color='purple', ls=':')
+plt.plot(x, np.exp(x), color='purple', ls=':', label='Exp')
+plt.legend()
+
+plt.xlabel("x")
+plt.ylabel("y")
 
 plt.savefig("build/exp.pdf")
