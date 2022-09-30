064231983ec9ea7f1121725d3b48ddb33743be52
Malte Zimmermann <malte.zimmermann@tu-dortmund.de>
2022-09-30 16:49:13 +0200

    Legend

--- plot.py
+++ plot.py
@@ -3,8 +3,14 @@ import numpy as np
 x = np.linspace(0, 10, 100)
 
 plt.plot(x, np.sin(x), "b--")
-plt.plot(x, 1/(x-1), "r--")
+plt.plot(x, 1/(x-1), "r--", label=r"$Juhu$")
 
 plt.ylim(0,1)
+
+plt.legend(loc="best")
+
 plt.savefig("build/plot.pdf")
+
+
+
 
